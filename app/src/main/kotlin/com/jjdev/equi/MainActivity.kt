a577a3b6303edb0385c771dda4db0d28d85744c4
Jan Jaroš <JanJarosDev@gmail.com>
2024-06-23 22:34:42 +0200

    feat(#75): Fixed status bar

--- app/src/main/kotlin/com/jjdev/equi/MainActivity.kt
+++ app/src/main/kotlin/com/jjdev/equi/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.jjdev.equi
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowCompat
+import com.jjdev.equi.ui.theme.EquiTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -14,8 +19,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Scaffold { contentPadding ->
-                DashboardScreen(modifier = Modifier.padding(contentPadding))
+            EquiTheme {
+                Scaffold { contentPadding ->
+                    DashboardScreen(modifier = Modifier.padding(contentPadding))
+                }
             }
         }
     }
--- app/src/main/kotlin/com/jjdev/equi/ui/theme/Theme.kt
+++ app/src/main/kotlin/com/jjdev/equi/ui/theme/Theme.kt
@@ -277,8 +277,7 @@ fun EquiTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
