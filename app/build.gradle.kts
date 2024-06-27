bc44a8a84ddd70e372ce93b1f22596ab5b256e70
Jan Jaroš <JanJarosDev@gmail.com>
2024-06-27 21:09:25 +0200

    feat(#102): Java up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,10 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
+
     kotlinOptions {
         jvmTarget = JVM_TARGET_VERSION
     }
