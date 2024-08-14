21ee19f1cbc5e1afccfb6c6ddbec0ee71a2b00e7
Jan Jaroš <JanJarosDev@gmail.com>
2024-08-14 18:04:08 +0200

    chore: UseCase tests

--- app/src/test/kotlin/com/jjdev/equi/RebalanceUseCaseTest.kt
+++ app/src/test/kotlin/com/jjdev/equi/RebalanceUseCaseTest.kt
@@ -7,49 +7,50 @@ import com.jjdev.equi.dashboard.domain.model.Investment
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 
 class RebalanceUseCaseTest {
 
     private val rebalanceUseCase = RebalanceUseCase()
 
-/*    @Test
+    @Test
     fun `rebalance should distribute investments equally when weights sum to 1`() = runTest {
         val investments = listOf(
-            Investment("A", 0.5, 100.0),
-            Investment("B", 0.5, 100.0)
+            Investment("A", BigDecimal.valueOf(0.5), BigDecimal.valueOf(100.0)),
+            Investment("B", BigDecimal.valueOf(0.5), BigDecimal.valueOf(100.0))
         )
 
-        rebalanceUseCase(Pair(100.0, investments)).onSuccess {
-            assertEquals(50.0, it[0].valueToInvest!!, 0.1)
-            assertEquals(150.0, it[0].targetValue!!, 0.1)
-            assertEquals(50.0, it[1].valueToInvest!!, 0.1)
-            assertEquals(150.0, it[1].targetValue!!, 0.1)
+        rebalanceUseCase(Pair(BigDecimal.valueOf(100.0), investments)).onSuccess {
+            assertEquals(BigDecimal.valueOf(50.0), it[0].valueToInvest!!)
+            assertEquals(BigDecimal.valueOf(150.0), it[0].targetValue!!)
+            assertEquals(BigDecimal.valueOf(50.0), it[1].valueToInvest!!)
+            assertEquals(BigDecimal.valueOf(150.0), it[1].targetValue!!)
         }
     }
 
     @Test
     fun `rebalance should handle uneven weights and distribute investments correctly`() = runTest {
         val investments = listOf(
-            Investment("A", 0.2, 200.0),
-            Investment("B", 0.8, 100.0)
+            Investment("A", BigDecimal.valueOf(0.2), BigDecimal.valueOf(200.0)),
+            Investment("B", BigDecimal.valueOf(0.8), BigDecimal.valueOf(100.0))
         )
 
-        rebalanceUseCase(Pair(1000.0, investments)).onSuccess {
-            assertEquals(60.0, it[0].valueToInvest!!, 0.1)
-            assertEquals(260.0, it[0].targetValue!!, 0.1)
-            assertEquals(940.0, it[1].valueToInvest!!, 0.1)
-            assertEquals(1040.0, it[1].targetValue!!, 0.1)
+        rebalanceUseCase(Pair(BigDecimal.valueOf(1000.0), investments)).onSuccess {
+            assertEquals(BigDecimal.valueOf(60.0), it[0].valueToInvest!!)
+            assertEquals(BigDecimal.valueOf(260.0), it[0].targetValue!!)
+            assertEquals(BigDecimal.valueOf(940.0), it[1].valueToInvest!!)
+            assertEquals(BigDecimal.valueOf(1040.0), it[1].targetValue!!)
         }
     }
 
     @Test
     fun `rebalance should throw exception when weights do not sum to 1`() = runTest {
         val investments = listOf(
-            Investment("A", 0.4, 100.0),
-            Investment("B", 0.5, 100.0)
+            Investment("A", BigDecimal.valueOf(0.4), BigDecimal.valueOf(100.0)),
+            Investment("B", BigDecimal.valueOf(0.5), BigDecimal.valueOf(100.0))
         )
 
-        rebalanceUseCase(Pair(100.0, investments)).onError {
+        rebalanceUseCase(Pair(BigDecimal.valueOf(100.0), investments)).onError {
             assertEquals("Weights must sum to 1.0", it.originalException.message)
         }
     }
@@ -57,75 +58,83 @@ class RebalanceUseCaseTest {
     @Test
     fun `rebalance should handle investments with zero weight`() = runTest {
         val investments = listOf(
-            Investment("A", 0.5, 100.0),
-            Investment("B", 0.0, 100.0),
-            Investment("C", 0.5, 100.0),
+            Investment("A", BigDecimal.valueOf(0.5), BigDecimal.valueOf(100.0)),
+            Investment("B", BigDecimal.valueOf(0.0), BigDecimal.valueOf(100.0)),
+            Investment("C", BigDecimal.valueOf(0.5), BigDecimal.valueOf(100.0)),
         )
 
-        rebalanceUseCase(Pair(100.0, investments)).onSuccess {
-            assertEquals(50.0, it[0].valueToInvest!!, 0.1)
-            assertEquals(150.0, it[0].targetValue!!, 0.1)
-            assertEquals(0.0, it[1].valueToInvest!!, 0.1)
-            assertEquals(100.0, it[1].targetValue!!, 0.1)
-            assertEquals(50.0, it[2].valueToInvest!!, 0.1)
-            assertEquals(150.0, it[2].targetValue!!, 0.1)
+        rebalanceUseCase(Pair(BigDecimal.valueOf(100.0), investments)).onSuccess {
+            assertEquals(BigDecimal.valueOf(50.0), it[0].valueToInvest!!)
+            assertEquals(BigDecimal.valueOf(150.0), it[0].targetValue!!)
+            assertEquals(BigDecimal.ZERO, it[1].valueToInvest!!)
+            assertEquals(BigDecimal.valueOf(100.0), it[1].targetValue!!)
+            assertEquals(BigDecimal.valueOf(50.0), it[2].valueToInvest!!)
+            assertEquals(BigDecimal.valueOf(150.0), it[2].targetValue!!)
         }
     }
 
     @Test
     fun `rebalance should correctly allocate funds without excess amount`() = runTest {
         val investments = listOf(
-            Investment("A", 0.5, 100.0),
-            Investment("B", 0.5, 100.0),
+            Investment("A", BigDecimal.valueOf(0.5), BigDecimal.valueOf(100.0)),
+            Investment("B", BigDecimal.valueOf(0.5), BigDecimal.valueOf(100.0)),
         )
 
-        rebalanceUseCase(Pair(100.0, investments)).onSuccess {
-            assertEquals(50.0, it[0].valueToInvest!!, 0.1)
-            assertEquals(150.0, it[0].targetValue!!, 0.1)
-            assertEquals(50.0, it[1].valueToInvest!!, 0.1)
-            assertEquals(150.0, it[1].targetValue!!, 0.1)
+        rebalanceUseCase(Pair(BigDecimal.valueOf(100.0), investments)).onSuccess {
+            assertEquals(BigDecimal.valueOf(50.0), it[0].valueToInvest!!)
+            assertEquals(BigDecimal.valueOf(150.0), it[0].targetValue!!)
+            assertEquals(BigDecimal.valueOf(50.0), it[1].valueToInvest!!)
+            assertEquals(BigDecimal.valueOf(150.0), it[1].targetValue!!)
         }
     }
 
     @Test
     fun `rebalance should reallocate excess amount correctly`() = runTest {
         val investments = listOf(
-            Investment("A", 0.5, 200.0),
-            Investment("B", 0.5, 100.0)
+            Investment("A", BigDecimal.valueOf(0.5), BigDecimal.valueOf(200.0)),
+            Investment("B", BigDecimal.valueOf(0.5), BigDecimal.valueOf(100.0))
         )
 
-        rebalanceUseCase(Pair(100.0, investments)).onSuccess {
-            assertEquals(0.0, it[0].valueToInvest!!, 0.1)
-            assertEquals(200.0, it[0].targetValue!!, 0.1)
-            assertEquals(100.0, it[1].valueToInvest!!, 0.1)
-            assertEquals(200.0, it[1].targetValue!!, 0.1)
+        rebalanceUseCase(Pair(BigDecimal.valueOf(100.0), investments)).onSuccess {
+            assertEquals(BigDecimal.ZERO, it[0].valueToInvest!!)
+            assertEquals(BigDecimal.valueOf(200.0), it[0].targetValue!!)
+            assertEquals(BigDecimal.valueOf(100.0), it[1].valueToInvest!!)
+            assertEquals(BigDecimal.valueOf(200.0), it[1].targetValue!!)
         }
     }
 
     @Test
     fun `rebalance should handle multiple adjustment cycles correctly`() = runTest {
         val investments = listOf(
-            Investment("A", 0.4, 500.0),
-            Investment("B", 0.3, 100.0),
-            Investment("C", 0.3, 200.0),
+            Investment("A", BigDecimal.valueOf(0.4), BigDecimal.valueOf(500.0)),
+            Investment("B", BigDecimal.valueOf(0.3), BigDecimal.valueOf(100.0)),
+            Investment("C", BigDecimal.valueOf(0.3), BigDecimal.valueOf(200.0)),
         )
 
-        rebalanceUseCase(Pair(150.0, investments)).onSuccess {
-            assertEquals(0.0, it[0].valueToInvest!!, 0.1)
-            assertEquals(500.0, it[0].targetValue!!, 0.1)
+        rebalanceUseCase(Pair(BigDecimal.valueOf(150.0), investments)).onSuccess {
+            assertEquals(BigDecimal.ZERO, it[0].valueToInvest!!)
+            assertEquals(BigDecimal.valueOf(500.0), it[0].targetValue!!)
 
-            assertEquals(125.0, it[1].valueToInvest!!, 0.1)
-            assertEquals(225.0, it[1].targetValue!!, 0.1)
+            assertEquals(BigDecimal.valueOf(125.0), it[1].valueToInvest!!)
+            assertEquals(BigDecimal.valueOf(225.0), it[1].targetValue!!)
 
-            assertEquals(25.0, it[2].valueToInvest!!, 0.1)
-            assertEquals(225.0, it[2].targetValue!!, 0.1)
+            assertEquals(BigDecimal.valueOf(25.0), it[2].valueToInvest!!)
+            assertEquals(BigDecimal.valueOf(225.0), it[2].targetValue!!)
 
             val totalFutureValue = investments.sumOf { investment -> investment.currentValue }
             assertEquals(
                 totalFutureValue,
-                it.sumOf { investment -> investment.targetValue!! },
-                0.1
+                it.sumOf { investment -> investment.targetValue!! }
             )
         }
-    }*/
+    }
+
+    @Test
+    fun `rebalance should not distribute if there are no investments`() = runTest {
+        val investments = emptyList<Investment>()
+
+        rebalanceUseCase(Pair(BigDecimal.valueOf(100.0), investments)).onSuccess {
+            assertEquals(investments, it)
+        }
+    }
 }
