91dbbe24802d7721a3711c1e680e154935155d1e
Jan Jaroš <JanJarosDev@gmail.com>
2024-08-16 16:54:04 +0200

    chore: reducer part 2

--- app/src/test/kotlin/com/jjdev/equi/dashboard/DashboardReducerTest.kt
+++ app/src/test/kotlin/com/jjdev/equi/dashboard/DashboardReducerTest.kt
@@ -1,6 +1,7 @@
 package com.jjdev.equi.dashboard
 
 import com.jjdev.equi.dashboard.presentation.DashboardReducer
+import com.jjdev.equi.dashboard.presentation.model.AddInvestmentDialogModel
 import com.jjdev.equi.dashboard.presentation.model.InvestmentUIModel
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.Dispatchers
@@ -17,6 +18,14 @@ import java.math.BigDecimal
 class DashboardReducerTest {
 
     private lateinit var reducer: DashboardReducer
+
+    private val investmentsUIModelListMock = persistentListOf(
+        InvestmentUIModel(
+            ticker = "AAPL",
+            weight = 0.5,
+            value = BigDecimal.valueOf(100.0)
+        )
+    )
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @BeforeEach
@@ -94,13 +103,7 @@ class DashboardReducerTest {
     fun `AddInvestment event should add investment to investments`() {
         assertEquals(
             DashboardReducer.DashboardState.initial().copy(
-                investments = persistentListOf(
-                    InvestmentUIModel(
-                        ticker = "AAPL",
-                        weight = 0.5,
-                        value = BigDecimal.valueOf(100.0)
-                    )
-                ),
+                investments = investmentsUIModelListMock
             ) to null,
             reducer.reduce(
                 previousState = DashboardReducer.DashboardState.initial().copy(
@@ -113,7 +116,138 @@ class DashboardReducerTest {
                 ),
             )
         )
-
     }
+
+    @Test
+    fun `Rebalance event should set investments to rebalancedInvestments`() {
+        assertEquals(
+            DashboardReducer.DashboardState.initial().copy(
+                investments = investmentsUIModelListMock
+            ) to null,
+            reducer.reduce(
+                previousState = DashboardReducer.DashboardState.initial().copy(
+                    investments = persistentListOf()
+                ),
+                event = DashboardReducer.DashboardEvent.Rebalance(
+                    rebalancedInvestments = investmentsUIModelListMock
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `UpdateDialogTicker event should update ticker in null addInvestmentDialogModel`() {
+        assertEquals(
+            DashboardReducer.DashboardState.initial().copy(
+                addInvestmentDialogModel = AddInvestmentDialogModel(
+                    ticker = "AAPL"
+                )
+                ) to null,
+            reducer.reduce(
+                previousState = DashboardReducer.DashboardState.initial().copy(
+                    addInvestmentDialogModel = null
+                ),
+                event = DashboardReducer.DashboardEvent.UpdateDialogTicker(
+                    ticker = "AAPL"
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `UpdateDialogValue event should update value in null addInvestmentDialogModel`() {
+        assertEquals(
+            DashboardReducer.DashboardState.initial().copy(
+                addInvestmentDialogModel = AddInvestmentDialogModel(
+                    value = "100"
+                )
+            ) to null,
+            reducer.reduce(
+                previousState = DashboardReducer.DashboardState.initial().copy(
+                    addInvestmentDialogModel = null
+                ),
+                event = DashboardReducer.DashboardEvent.UpdateDialogValue(
+                    value = "100"
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `UpdateDialogWeight event should update weight in null addInvestmentDialogModel`() {
+        assertEquals(
+            DashboardReducer.DashboardState.initial().copy(
+                addInvestmentDialogModel = AddInvestmentDialogModel(
+                    weight = "0.5"
+                )
+            ) to null,
+            reducer.reduce(
+                previousState = DashboardReducer.DashboardState.initial().copy(
+                    addInvestmentDialogModel = null
+                ),
+                event = DashboardReducer.DashboardEvent.UpdateDialogWeight(
+                    weight = "0.5"
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `UpdateDialogTicker event should update ticker in non null addInvestmentDialogModel`() {
+        assertEquals(
+            DashboardReducer.DashboardState.initial().copy(
+                addInvestmentDialogModel = AddInvestmentDialogModel(
+                    ticker = "AAPL"
+                )
+            ) to null,
+            reducer.reduce(
+                previousState = DashboardReducer.DashboardState.initial().copy(
+                    addInvestmentDialogModel = AddInvestmentDialogModel(ticker = "GOOGL")
+                ),
+                event = DashboardReducer.DashboardEvent.UpdateDialogTicker(
+                    ticker = "AAPL"
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `UpdateDialogValue event should update value in non null addInvestmentDialogModel`() {
+        assertEquals(
+            DashboardReducer.DashboardState.initial().copy(
+                addInvestmentDialogModel = AddInvestmentDialogModel(
+                    value = "100"
+                )
+            ) to null,
+            reducer.reduce(
+                previousState = DashboardReducer.DashboardState.initial().copy(
+                    addInvestmentDialogModel = AddInvestmentDialogModel(value = "50")
+                ),
+                event = DashboardReducer.DashboardEvent.UpdateDialogValue(
+                    value = "100"
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `UpdateDialogWeight event should update weight in non null addInvestmentDialogModel`() {
+        assertEquals(
+            DashboardReducer.DashboardState.initial().copy(
+                addInvestmentDialogModel = AddInvestmentDialogModel(
+                    weight = "0.5"
+                )
+            ) to null,
+            reducer.reduce(
+                previousState = DashboardReducer.DashboardState.initial().copy(
+                    addInvestmentDialogModel = AddInvestmentDialogModel(weight = "0.1")
+                ),
+                event = DashboardReducer.DashboardEvent.UpdateDialogWeight(
+                    weight = "0.5"
+                )
+            )
+        )
+    }
+
 
 }
